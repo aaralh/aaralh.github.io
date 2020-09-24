8797c853089d2e5770b67e1adc945306cc782ccf
Aaro Alhainen <aaro.alhainen@gmail.com>
2020-09-24 10:37:21 +0300

    Create footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<style>
+.footer {
+	height: 30px;
+	width: 100vw;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 12px;
+}
+</style>
+
+<div class="footer">
+	<div class="copyright">
+		© 2020 Aaro Alhainen
+	</div>
+</div>
