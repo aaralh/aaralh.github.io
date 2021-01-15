619b4d643eb7374b788275e196c6ec18d5e5e6ee
Aaro Alhainen <aaro.alhainen@gmail.com>
2021-01-15 21:54:14 +0200

    Change retainable.io to twilik.com

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -6,12 +6,12 @@
     if (container) {
       var css = document.createElement("link");
       css.href =
-        "https://www.retainable.io/assets/retainable/rss-embed/retainable.css";
+        "https://www.twilik.com/assets/retainable/rss-embed/retainable.css";
       css.rel = "stylesheet";
       document.getElementsByTagName("head")[0].appendChild(css);
       var script = document.createElement("script");
       script.src =
-        "https://www.retainable.io/assets/retainable/rss-embed/retainable.js";
+        "https://www.twilik.com/assets/retainable/rss-embed/retainable.js";
       document.getElementsByTagName("body")[0].appendChild(script);
     }
   });
